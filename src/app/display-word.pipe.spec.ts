06ce5f77f83ca1fb650b5c354eaf017e9ed99f89
nachomonllor <nachomonllorc@gmail.com>
2025-09-28 00:55:51 -0300

    ajustes ahorcado

--- src/app/display-word.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisplayWordPipe } from './display-word.pipe';
-
-describe('DisplayWordPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DisplayWordPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
