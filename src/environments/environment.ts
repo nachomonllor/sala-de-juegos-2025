92ad2ee88ce1bdb89d5be2bd25b0cce12fc845af
nachomonllor <nachomonllorc@gmail.com>
2025-11-24 14:28:31 -0300

    supabaseKay

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   // Supabase Cloud (entorno por defecto)
   supabaseUrl: 'https://tuwlrspqlkpqatnaintx.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1d2xyc3BxbGtwcWF0bmFpbnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NDcyOTEsImV4cCI6MjA3MTMyMzI5MX0.O5eawMd27SKifzyOvKp5fJZcvgBodxXA5LZWZdexRSA',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1d2xyc3BxbGtwcWF0bmFpbnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NDcyOTEsImV4cCI6MjA3MTMyMzI5MX0.O5eawMd27SKifzyOvKp5fJZcvgBodxXA5LZWZdexRSA',
   // Si necesitás volver a la instancia local, reemplazá estas variables por las de localhost.
 
   accesoRapidoHabilitado: true,
